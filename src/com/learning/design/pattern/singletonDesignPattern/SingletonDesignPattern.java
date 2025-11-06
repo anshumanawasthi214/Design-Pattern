dfd95daab73a02eec8bdab83ae7c19b28b638951
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-11-06 20:26:57 +0530

    Added Eager Initialization way to create singleton Object

--- src/com/learning/design/pattern/singletonDesignPattern/SingletonDesignPattern.java
+++ src/com/learning/design/pattern/singletonDesignPattern/SingletonDesignPattern.java
@@ -20,14 +20,11 @@ public class SingletonDesignPattern {
 	
 	//Eager way of object creation
 	
-	private static SingletonDesignPattern  eagerObject; //static variables don't need to initialize with any value default is null
+	private static SingletonDesignPattern  eagerObject=new SingletonDesignPattern(); //static variables don't need to initialize with any value default is null
 	
 			private SingletonDesignPattern() {}//making constructor private so it can't be accessible;
 			
 			public static SingletonDesignPattern getObject(){//Using Getter to create object
-				if(eagerObject==null) {
-					eagerObject=new  SingletonDesignPattern();
-				}
 					return eagerObject;
 				
 			}
