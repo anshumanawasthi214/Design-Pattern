ef9f53b181957a66e200670f15adcf5f15515080
anshumanawasthi214 <awasthiengineer214@gmail.com>
2025-12-04 20:39:31 +0530

    written deep copy logic in prototype design pattern

--- src/com/learning/design/pattern/PrototypeDesignPattern/NetworkConnection.java
+++ src/com/learning/design/pattern/PrototypeDesignPattern/NetworkConnection.java
@@ -1,14 +1,25 @@
 package com.learning.design.pattern.PrototypeDesignPattern;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NetworkConnection implements Cloneable{
 
 	@Override
 	protected Object clone() throws CloneNotSupportedException {
-		// TODO Auto-generated method stub
-		return super.clone();
+		//Logic For Cloning
+		NetworkConnection conn=new NetworkConnection();
+		conn.setIp(this.getIp());
+		conn.setImportantData(this.importantData);
+		for(String s: this.getDomains()) {
+			conn.getDomains().add(s);
+		}
+		
+		return conn;
 	}
 	private String ip;
 	private String importantData;
+	private List<String> domains=new ArrayList<>();
 	
 	public String getImportantData() {
 		return importantData;
@@ -19,11 +30,26 @@ public class NetworkConnection implements Cloneable{
 	
 	@Override
 	public String toString() {
-		return "NetworkConnection [ip=" + ip + ", importantData=" + importantData + "]";
+		return "NetworkConnection [ip=" + ip + ", importantData=" + importantData + ", domains=" + domains + "]";
 	}
 	public void loadVeryImportantData() throws InterruptedException {
 		this.importantData="Very Very Important Data";//Suppose it takes lots of time to load around 5 min
+		domains.add("google.com");
+		domains.add("yahoo.com");
+		domains.add("youtube.com");
+		domains.add("meta.com");
+		domains.add("facebook.com");
+		domains.add("instagram.com");
+		domains.add("whatsapp.com");
+		domains.add("gmail.com");
+
 		Thread.sleep(5000);
+	}
+	public List<String> getDomains() {
+		return domains;
+	}
+	public void setDomains(List<String> domains) {
+		this.domains = domains;
 	}
 	public String getIp() {
 		return ip;
